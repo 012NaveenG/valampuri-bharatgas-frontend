0f37206ab4b78148ae92cf05fd2f8e88849eb52e
012NaveenG <012guptagkp@gmail.com>
2024-12-25 20:51:19 +0530

    changes vite config

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000'
+        // target: 'http://localhost:4000'
+        target: 'https://valampuri-bharatgas-backend.vercel.app/'
       }
     },
     host: true, // Or specify the IP address like '192.168.x.x'
