363d3bd8df3d87af9072d4bf596ebfedb68b299d
012NaveenG <012guptagkp@gmail.com>
2025-01-01 15:32:10 +0530

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://valampuri-bharatgas-backend.onrender.com',
-        target: 'http://localhost:4000/',
+        target: 'https://valampuri-bharatgas-backend.onrender.com',
+        // target: 'http://localhost:4000/',
         changeOrigin: true,
         secure: false, 
       }
